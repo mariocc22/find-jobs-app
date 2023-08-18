2696c9d33cc5b9c930f535a5bfcf4b9a90962dc7
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-08-17 22:37:31 -0700

    Initial Commit

---
+++ app/_layout.js
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// this is when the app is loading it will make sure the splash screen is not hidden
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // when the app is loaded it will hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView}></Stack>;
+};
+
+export default Layout;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/mario.png";
+
+export default {
+  profile,
+};
